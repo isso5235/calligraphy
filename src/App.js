46262d7e4002a3a03c23fc1651d8478d3b8604e0
isso5235 <gakkouyou7841@gmail.com>
2023-04-13 01:08:01 +0900

    feat: paint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { ReactPainter } from 'react-painter';
+import {useRef} from 'react';
+
 
 function App() {
+  const canvasRef = useRef(null);
+  const clearCanvas = () => {
+    window.location.reload();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactPainter
+            width={500}
+            height={500}
+            initialLineWidth={20}
+            ref={canvasRef}
+            render={({ canvas }) => (
+          <div>
+            <div className="Title">GMDay 今年の一文字</div>
+            <div className="Paint">{canvas}</div>
+          </div>
+        )}
+      />
+      <button onClick={() => (clearCanvas())}>
+        白紙
+      </button>
     </div>
   );
 }
