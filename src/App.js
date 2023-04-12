ae97e131229f6d79ef73fd2c178f579326722e91
isso5235 <gakkouyou7841@gmail.com>
2023-04-13 01:56:41 +0900

    add env

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import { ReactPainter } from 'react-painter';
-import {useRef} from 'react';
-
 
 function App() {
-  const canvasRef = useRef(null);
   const clearCanvas = () => {
     window.location.reload();
   };
@@ -16,7 +12,6 @@ function App() {
             width={500}
             height={500}
             initialLineWidth={20}
-            ref={canvasRef}
             render={({ canvas }) => (
           <div>
             <div className="Title">GMDay 今年の一文字</div>
